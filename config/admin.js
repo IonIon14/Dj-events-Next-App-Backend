9d37522d92b4d3bc7e0facf74242d55d9cfd2c61
Ionut Baciu <ionut.baciu1501@stud.etti.upb.ro>
2022-03-16 21:06:29 +0200

    set up strapi route

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'fc4f0e47f6c3aebe81d3c6ce122c7b5d'),
+    secret: env("ADMIN_JWT_SECRET", "fc4f0e47f6c3aebe81d3c6ce122c7b5d"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "56ed1702db4fcc6037e34b9b283bf2d7"),
   },
 });
